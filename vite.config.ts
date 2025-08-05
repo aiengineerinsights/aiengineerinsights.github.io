742b3e8f06667b148fd9a242ad79b7cc56682fa4
aiengineerinsights <aiengineerinsights@gmail.com>
2025-08-05 22:45:40 +0530

    prevent error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/aiengineerinsights.github.io/",
   server: {
     host: "::",
     port: 8080,
