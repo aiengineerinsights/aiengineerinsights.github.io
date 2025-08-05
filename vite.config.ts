c043f2c8a39a3006c1191e5497cf475e83fcdcdf
aiengineerinsights <aiengineerinsights@gmail.com>
2025-08-05 22:50:17 +0530

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/aiengineerinsights.github.io/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
